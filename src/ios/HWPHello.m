5cb140f869e359d7c13668f3a60a859f141d53a8
harikrishnabista <harikrista@gmail.com>
2017-03-22 23:52:46 -0500

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -1,10 +1,16 @@
 #import "HWPHello.h"
+#import <TopHackerNews/TopHackerNewsSDK.h>
 
 @implementation HWPHello
 
 - (void)greet:(CDVInvokedUrlCommand*)command
 {
 
+    TopHackerNewsSDK *sdk = [[TopHackerNewsSDK alloc] init];
+    
+    [sdk iniSDK];
+    [sdk showHackerNews];
+    
     NSString* name = [[command arguments] objectAtIndex:0];
     NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
 
